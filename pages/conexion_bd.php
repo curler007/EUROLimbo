626fef360c0cd6e2247924cd7555a1c9d127bceb
altapia <altapia@gmail.com>
2012-06-14 11:21:46 +0300

    Modificado

--- pages/conexion_bd.php
+++ pages/conexion_bd.php
@@ -1,10 +1,8 @@
 <?
 date_default_timezone_set('Europe/Madrid');
 //Conexion con la base
-//mysql_connect("localhost:3306","tapia","1234"); 
-mysql_connect("db418419046.db.1and1.com:3306","dbo418419046","anopeludo"); 
+mysql_connect("localhost:3306","tapia","1234"); 
 
 //selecciÃ³n de la base de datos con la que vamos a trabajar 
-//mysql_select_db("EURO2012"); 
-mysql_select_db("db418419046"); 
+mysql_select_db("EURO2012"); 
 ?>
